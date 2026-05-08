8f22b306566625358f1968c6dbf6a0d9a0fa1726
vibe-chung <vibechung@proton.me>
2026-05-08 21:23:39 +0100

    feat: Add date formatting to AmexParser

--- internal/parser/amex.go
+++ internal/parser/amex.go
@@ -11,5 +11,9 @@ type AmexParser struct{}
 
 // Parse implements Parser for Amex CSV files.
 func (AmexParser) Parse(r io.Reader, filename string) ([]string, error) {
-	return parseCSV(r, filename)
+	return parseCSVWithTransform(r, filename, func(record map[string]interface{}) {
+		if v, ok := record["date"].(string); ok {
+			record["date"] = reformatDateDDMMYYYY(v)
+		}
+	})
 }
